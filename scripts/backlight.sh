33a31ec4b44585dae22e503f329e5fc5a4cb5698
tonrl <hoswt4s3321399@gmail.com>
2025-05-29 19:40:43 +0900

    Fix backlight handlig script

--- scripts/backlight.sh
+++ scripts/backlight.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-icon_base="$HOME/.local/share/icons/ePapirus/32x32/"
+readonly icon_base="$HOME/.local/share/icons/ePapirus/32x32/"
 high_icon="$icon_base/panel/brightness-high-symbolic.svg"
 mid_icon="$icon_base/panel/brightness-symbolic.svg"
 low_icon="$icon_base/panel/brightness-low-symbolic.svg" 
@@ -9,46 +9,54 @@ expireTime=1000
 
 swaync_op="-h string:x-canonical-private-synchronous:brightness_notif"
 
+command -v brightnessctl >/dev/null 2>&1 || {
+        echo "brightnessctl is required but not installed." >&2
+        exit 1
+}
+
 function get_brightness {
         brightness=$(brightnessctl get)
         max_brightness=$(brightnessctl max)
         echo $((brightness * 100 / max_brightness))
 }
-function notify_send {
-        notify-send -a "System" -i "${high_icon}" -r 2593 -u low $swaync_op "Brightness: ${brightness}" "$bar" -t $expireTime
+
+function notification_fn {
+        local icon_op="$1"
+        notify-send -a "System" -i "${icon_op}" -r 2593 -u low ${swaync_op} "Helligkeit: ${brightness}" "$bar" -t $expireTime
 }
 
 function send_notification {
 
-        brightness=$(get_brightness)
-        brightness=${brightness%%.*}
+        local brightness="$1"
 
         if [ "$brightness" -gt 0 ]; then
-                bar=$(seq -s "─" $(($brightness / 3)) | sed 's/[0-9]//g')"─"
+                bar=$(printf '─%.0s' $(seq 1 "$((brightness / 3))"))
         else
                 bar=""
         fi
 
-        if [ "$brightness" -ge 75 ]; then
-                notify-send -a "System" -i "${high_icon}" -r 2593 -u low $swaync_op "Brightness: ${brightness}" "$bar" -t $expireTime
-
-        elif [ "$brightness" -ge 50 ]; then
-                notify-send -a "System" -i "${mid_icon}" -r 2593 -u low $swaync_op "Brightness: ${brightness}" "$bar" -t $expireTime
-        
-        elif [ "$brightness" -ge 20 ]; then
-                notify-send -a "System" -i "${low_icon}" -r 2593 -u low $swaync_op "Brightness: ${brightness}" "$bar" -t $expireTime
-
-        elif [ "$brightness" -eq 0 ]; then
-                notify-send -a "System" -i "${off_icon}" -r 2593 -u low $swaync_op "Brightness: ${brightness}" "Backlight off" -t $expireTime
+        if [ "$brightness" -eq 0 ]; then
+                notify-send -a "System" -i "${off_icon}" -r 2593 -u low $swaync_op "Helligkeit: ${brightness}" "Backlight off" -t $expireTime
         else
-                notify-send -a "System" -i "${off_icon}" -r 2593 -u low $swaync_op "Brightness: ${brightness}" "$bar" -t $expireTime
-        fi  
+                if [ "$brightness" -ge 75 ]; then
+                        icon_op=${high_icon}
+
+                elif [ "$brightness" -ge 50 ]; then
+                        icon_op=${mid_icon}
+
+                elif [ "$brightness" -ge 20 ]; then
+                        icon_op=${low_icon}
+
+                else
+                        icon_op=${off_icon}
+                fi  
+                notification_fn "$icon_op"
+        fi
 
 }
 
 function brightness_up {
-        brightness=$(get_brightness)
-        brightness=${brightness%%.*}
+        local brightness=$(get_brightness)
 
         if [ "$brightness" -lt 1 ]; then
                 brightnessctl -qe s +1%
@@ -57,10 +65,10 @@ function brightness_up {
         else
                 brightnessctl -q s +5%
         fi
+        send_notification "$(get_brightness)"
 }
 function brightness_down {
-        brightness=$(get_brightness)
-        brightness=${brightness%%.*}
+        local brightness=$(get_brightness)
 
         if [ "$brightness" -le 1 ]; then
                 brightnessctl -qe s 1%-
@@ -69,16 +77,19 @@ function brightness_down {
         else
                 brightnessctl -q s 5%-
         fi
+        send_notification "$(get_brightness)"
 }
 
 case $1 in
         up)
                 brightness_up
-                send_notification
                 ;;
         down)
                 brightness_down
-                send_notification
+                ;;
+        *)
+                echo "Usage: $0 {up|down}"
+                exit 1
                 ;;
 esac
 
