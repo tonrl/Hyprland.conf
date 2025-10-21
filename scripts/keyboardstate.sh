1de15bff27b79f7ce627a08a3b87b7bacc96a38e
tonrl <hoswt4s3321399@gmail.com>
2025-10-21 09:31:33 +0900

    Minor changes

--- scripts/keyboardstate.sh
+++ scripts/keyboardstate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function get_input {
-        [[ $(fcitx5-remote) -eq 2 ]] &&echo "日本語" || echo "EN" 
+        [[ $(fcitx5-remote) -eq 2 ]] &&echo "あ" || echo "EN" 
 }
 
 function change_input {
