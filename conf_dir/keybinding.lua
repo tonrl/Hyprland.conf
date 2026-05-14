6592b7c4513dd39cd6e349d4d73a80174b1767e8
tonrl <hoswt4s3321399@gmail.com>
2026-05-14 19:21:43 +0900

    Small Changes keybinding

--- conf_dir/keybinding.lua
+++ conf_dir/keybinding.lua
@@ -93,6 +93,11 @@ hl.bind(mainMod .. " + SHIFT + 0", hl.dsp.window.move({ workspace = 10 }))
 hl.bind(" + CTRL + ALT + H", hl.dsp.focus({ workspace = "e-1"}))
 hl.bind(" + CTRL + ALT + L", hl.dsp.focus({ workspace = "e+1"}))
 
+
+-- Move/resize windows with mainMod + LMB/RMB and dragging
+hl.bind("ALT + mouse:272", hl.dsp.window.drag(), { mouse = true })    -- ALT + LMB: Move a window by dragging more than 10px.
+hl.bind("ALT + mouse:272", hl.dsp.window.resize(), { mouse = true })  -- ALT + LMB: Floats a window by clicking
+
 --Scroll through existing workspaces with mainMod + scroll
 hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1"}))
 hl.bind(mainMod .. " + mouse_up", hl.dsp.focus({ workspace = "e-1"}))
