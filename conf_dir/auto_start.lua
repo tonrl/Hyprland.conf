623cd0c621eacef163744f6dfb384eb9e8137163
tonrl <hoswt4s3321399@gmail.com>
2026-05-16 12:24:29 +0900

    Some commit

--- conf_dir/auto_start.lua
+++ conf_dir/auto_start.lua
@@ -6,3 +6,11 @@
 hl.on("hyprland.start", function()
         hl.exec_cmd("uwsm app -- auto_start.sh")
 end)
+
+-- hl.on("window.open", function()
+--         hl.exec_cmd("paplay ~/.local/share/sounds/win-sounds/'Windows XP Critical Stop.mp3'")
+-- end)
+--
+-- hl.on("hyprland.shutdown", function()
+--         hl.exec_cmd("paplay ~/.local/share/sounds/win-sounds/'Windows XP Shutdown.mp3' && sleep 2")
+-- end)
